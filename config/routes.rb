a5c9913d9bd6e82fa3243921d7055c3f31098971
kugyu10 <kugyu10@gmail.com>
2017-12-22 07:37:51 +0900

    レイアウトとRootesを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/edit'
+
   root 'static_pages#home'
 
-  get 'static_pages/about'
-  get 'static_pages/home'
-  get 'static_pages/help'
+  get '/about', to:'static_pages#about'
+  get '/home', to:'static_pages#home'
+  get '/help', to:'static_pages#help'
 
 end
