32586412f71d08f82ffa52b6fe33f9435b03cd63
kugyu10 <kugyu10@gmail.com>
2018-04-25 14:25:47 +0900

    S3利用

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -5,12 +5,14 @@ class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [400,400]
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # Choose what kind of storage to use for this uploader
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
+  # Update先dir
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
